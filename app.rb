6a33f7608111d5ca8ca304cd0ec20b78af7014ee
Huy Do <huyddo40@gmail.com>
2019-09-10 15:59:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		# "<h1>Hello Wordl</h1>"
+		erb :index
 	end
+
+	get "/info" do
+		# "Testng the info page"
+		erb :info
+	end
+
 end
